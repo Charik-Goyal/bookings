c1db47c49d09b90735e9250ddd6ea4741c145c9c
charik-goyal <charikgoyal8@gmail.com>
2022-10-29 13:08:05 +0530

    Added the image folder,file and wrote logic for handling those static images

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,11 +13,14 @@ func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 
 	mux.Use(middleware.Recoverer)
-	mux.Use(WriteToConsole)
+	// mux.Use(WriteToConsole)
 	mux.Use(NoSurf)
 	mux.Use(SessionLoad)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 	return mux
 }
