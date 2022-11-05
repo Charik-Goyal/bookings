6f0cae07ad5bf8025151bc3a969a5ffd18055b3c
charik-goyal <charikgoyal8@gmail.com>
2022-11-05 23:50:30 +0530

    Server validation, Session handler (take session one page to another) ,create reservation-summary page

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -29,6 +29,8 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/contact", handlers.Repo.Contact)
 
 	mux.Get("/make-reservation", handlers.Repo.Reservation)
+	mux.Post("/make-reservation", handlers.Repo.PostReservation)
+	mux.Get("/reservation-summary", handlers.Repo.ReservationSummary)
 
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
